b64a2ac0c79dcd94de212cf5ea4afd0094b3321b
V神 <41903645@qq.com>
2015-09-27 13:19:48 +0800

    add a post

--- js/nav-toggle.js
+++ js/nav-toggle.js
@@ -1,13 +1,1 @@
-$(document).ready(function() {
-  $('.site-nav-toggle button').on('click', function() {
-    var $siteNav = $('.site-nav');
-    var ON_CLASS_NAME = 'site-nav-on';
-    var isSiteNavOn = $siteNav.hasClass(ON_CLASS_NAME);
-    var animateAction = isSiteNavOn ? 'slideUp' : 'slideDown';
-    var animateCallback = isSiteNavOn ? 'removeClass' : 'addClass';
-
-    $siteNav.stop()[animateAction]('fast', function() {
-      $siteNav[animateCallback](ON_CLASS_NAME);
-    });
-  });
-});
+$(document).ready(function(){$(".site-nav-toggle button").on("click",function(){var a=$(".site-nav");var n="site-nav-on";var s=a.hasClass(n);var t=s?"slideUp":"slideDown";var o=s?"removeClass":"addClass";a.stop()[t]("fast",function(){a[o](n)})})});
